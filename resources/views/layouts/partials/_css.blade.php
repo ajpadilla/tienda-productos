6955b97a89dfbaf85395af22f1310beb6405365b
ajpadilla <ajpadilla88@gmail.com>
2016-03-05 17:35:27 -0430

    Agregando blade para insertar link de estilos al proyecto

--- resources/views/layouts/partials/_css.blade.php
+++ resources/views/layouts/partials/_css.blade.php
@@ -1,3 +1,5 @@
 <!-- FONTS -->
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/styles.css') }}">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
