97b817e8e31e9055b3b68986954aaa0c3097b462
ajpadilla <ajpadilla88@gmail.com>
2016-03-14 22:40:03 -0430

    Agregando directorio Principal del proyecto para clases modelos y clases tipo reposotiry

---
+++ app/Store/Product/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Store\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function mostrar()
+    {
+    	return "Hola mundo";
+    }
+}
---
+++ app/Store/Product/ProductRepository.php
@@ -0,0 +1,12 @@
+<?php  
+	namespace App\Store\Product;
+
+	/**
+	* 
+	*/
+	class ProductRepository 
+	{
+		
+	
+	}
+?>
