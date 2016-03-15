b0cf726836b4edd711bbc0d0ab7fe50b92fc9371
ajpadilla <ajpadilla88@gmail.com>
2016-03-14 23:03:44 -0430

    Incluyendo ruta a vue.js y a vue-strap.js

--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,2 +1,4 @@
 <script type="text/javascript"  src="{{ URL::asset('js/jquery-2.2.1.min.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('js/vue.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('js/node_modules/vue-strap/dist/vue-strap.min.js') }}"></script>
 <script type="text/javascript"  src="{{ URL::asset('css/bootstrap/js/bootstrap.min.js') }}"></script>
