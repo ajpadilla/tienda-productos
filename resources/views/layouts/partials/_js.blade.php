b2141bf0d9a3c031d926f5cb79bd994fecb39efc
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 09:27:13 -0430

    Reorganizando inclides

--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,4 +1,6 @@
 <script type="text/javascript"  src="{{ URL::asset('js/jquery-2.2.1.min.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('css/bootstrap/js/bootstrap.min.js') }}"></script>
 <script type="text/javascript"  src="{{ URL::asset('js/vue.js') }}"></script>
 <script type="text/javascript"  src="{{ URL::asset('js/node_modules/vue-strap/dist/vue-strap.min.js') }}"></script>
-<script type="text/javascript"  src="{{ URL::asset('css/bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('js/app.js') }}"></script>
+
