86667058977d20c3f9a78271248bd5207b3ab2f4
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 17:46:00 -0430

    Agregando constructo a la clase junto con metodo store

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -1,14 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use App\Store\Classification\ClassificationRepository;
+use App\Http\Requests\StoreClassificationRequest;
 
 class ClassificationController extends Controller
 {
-    //
+
+	protected $userRepository;
+
+    public function __construct(UserRepository $userRepository) {
+    	$this->userRepository = $userRepository;
+    }
+
+    public function index()
+    {
+    	return view('classification.index');
+    }
+
+    public function store(StoreClassificationRequest $request)
+    {
+    	return response()->json(compact('user'));
+    }
 }
