1f6091e0005623440e796b0361d2aeb91fc39abc
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 09:26:48 -0430

    Modificando contenido del blade

--- resources/views/layouts/partials/_content.blade.php
+++ resources/views/layouts/partials/_content.blade.php
@@ -1,3 +1,7 @@
-<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('content')
 	<h1 class="page-header">Dashboard</h1>
-</div>
+@endsection
