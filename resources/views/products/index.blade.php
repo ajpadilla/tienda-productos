8d57a38afd2346c8a5b51ab355e1de9583d62ed4
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 09:28:49 -0430

    Agregando carpeta para almacenar blade para modelo procudto

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('title', 'Products')
+
+@section('content')
+<button class="btn btn-default top-head" v-on:click="showCustomModal = true">
+  Show custom modal
+</button>
+<modal :show.sync="showCustomModal" effect="fade" width="400">
+	<div slot="modal-header" class="modal-header">
+		<h4 class="modal-title">
+			<i>Custom</i> <code>Modal</code> <b>Title</b>
+		</h4>
+	</div>
+	<div slot="modal-body" class="modal-body">Algo</div>
+	<div slot="modal-footer" class="modal-footer">
+		<button type="button" class="btn btn-default" @click='showCustomModal = false'>Exit</button>
+		<button type="button" class="btn btn-success" @click='showCustomModal = false'>Custom Save</button>
+	</div>
+</modal>
+@endsection
+
