8e2ebe874e4738e8d261eb9a333438be1ad07663
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 17:46:25 -0430

    Herendando de la clase base repository

--- app/Store/Classification/ClassificationRepository.php
+++ app/Store/Classification/ClassificationRepository.php
@@ -1,11 +1,17 @@
 <?php  
 	namespace App\Store\Classification;
 
+	use App\Store\Base\BaseRepository;
+	use App\Store\Classification\Classification;
+
 	/**
 	* 
 	*/
-	class ClassificationRepository
+	class ClassificationRepository extends BaseRepository
 	{
-
+		function __construct()
+		{
+			$this->setModel(new Classification);
+		}
 	}
 ?>
