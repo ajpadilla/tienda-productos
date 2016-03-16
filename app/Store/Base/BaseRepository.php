f234ff64a5210a38dd70749568b6ea29846f1f1e
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 10:01:35 -0430

    Agregando metodos a clase

--- app/Store/Base/BaseRepository.php
+++ app/Store/Base/BaseRepository.php
@@ -1,10 +1,51 @@
 <?php 
-	namespace App\Store\Base;
+namespace App\Store\Base;
 
+class BaseRepository 
+{
+	protected $model;
 
-	class BaseRepository 
+	public function setModel($model)
 	{
-		
+		$this->model = $model;
 	}
 
- ?>
+	public function getModel()
+	{
+		return $this->model;
+	}
+
+	public function create($data = array())
+	{
+		$model = $this->model->create($data); 
+		return $model;
+	}
+
+	public function  getAll($exclude = null)
+	{
+		if($exclude)
+			return $this->getModel()->whereNotIn('id', $exclude)->get();
+		return $this->getModel()->all();
+	}	
+
+	public function getAllForSelect()
+	{
+		return $this->getAll()->lists('name', 'id');
+	}
+
+	public function delete($id)
+	{
+		$model = $this->get($id); 
+		return $model->delete();
+	}
+
+	public function get($id)
+	{
+		return $this->model->findOrFail($id);
+	}	
+
+	public function update($data = array()){}
+
+}
+
+?>
