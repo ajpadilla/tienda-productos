79a4b9326236f5c267b3bc9830b9eaacf12b62a7
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 17:49:40 -0430

    Agregando sección para clasificaciíon

--- resources/views/layouts/partials/_sidebar.blade.php
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -1,50 +1,48 @@
-<div class="col-sm-3 col-md-2">
-	<div class="nav-side-menu">
-		<div class="brand">Brand Logo</div>
-		<i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
-		<div class="menu-list">
-			<ul id="menu-content" class="menu-content collapse in">
-				<li>
-					<a href="#">
-						<i class="fa fa-dashboard fa-lg"></i> Dashboard
-					</a>
-				</li>
-				<li  data-toggle="collapse" data-target="#products" class="collapsed active">
-					<a href="#"><i class="fa fa-gift fa-lg"></i>Products<span class="arrow"></span></a>
-				</li>
-				<ul class="sub-menu collapse" id="products">
-					<li class="active"><a href="#">Index</a></li>
-				</ul>
-
-				<li data-toggle="collapse" data-target="#service" class="collapsed">
-					<a href="#"><i class="fa fa-globe fa-lg"></i> Classification <span class="arrow"></span></a>
-				</li>  
-				<ul class="sub-menu collapse" id="service">
-					<li>Index</li>
-				</ul>
-
-
-				<li data-toggle="collapse" data-target="#new" class="collapsed">
-					<a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
-				</li>
-				<ul class="sub-menu collapse" id="new">
-					<li>New New 1</li>
-					<li>New New 2</li>
-					<li>New New 3</li>
-				</ul>
-
-				<li>
-					<a href="#">
-						<i class="fa fa-user fa-lg"></i> Profile
-					</a>
-				</li>
-
-				<li>
-					<a href="#">
-						<i class="fa fa-users fa-lg"></i> Users
-					</a>
-				</li>
+<div class="nav-side-menu">
+	<div class="brand">Brand Logo</div>
+	<i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+	<div class="menu-list">
+		<ul id="menu-content" class="menu-content collapse in">
+			<li>
+				<a href="#">
+					<i class="fa fa-dashboard fa-lg"></i> Dashboard
+				</a>
+			</li>
+			<li  data-toggle="collapse" data-target="#products" class="collapsed active">
+				<a href="#"><i class="fa fa-gift fa-lg"></i>Products<span class="arrow"></span></a>
+			</li>
+			<ul class="sub-menu collapse" id="products">
+				<li class="active"><a href="#">Index</a></li>
 			</ul>
-		</div>
+
+			<li data-toggle="collapse" data-target="#service" class="collapsed">
+				<a href="#"><i class="fa fa-globe fa-lg"></i> Classification <span class="arrow"></span></a>
+			</li>  
+			<ul class="sub-menu collapse" id="service">
+				<li>Index</li>
+			</ul>
+
+
+			<li data-toggle="collapse" data-target="#new" class="collapsed">
+				<a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
+			</li>
+			<ul class="sub-menu collapse" id="new">
+				<li>New New 1</li>
+				<li>New New 2</li>
+				<li>New New 3</li>
+			</ul>
+
+			<li>
+				<a href="#">
+					<i class="fa fa-user fa-lg"></i> Profile
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					<i class="fa fa-users fa-lg"></i> Users
+				</a>
+			</li>
+		</ul>
 	</div>
 </div>
