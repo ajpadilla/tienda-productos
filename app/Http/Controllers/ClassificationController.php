037cb44aea07fc29c376bc7b239bb4d3100ac3ea
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 10:07:49 -0430

    Agregando controlador para modelo Classification for products

---
+++ app/Http/Controllers/ClassificationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Store\Classification\ClassificationRepository;
+
+class ClassificationController extends Controller
+{
+    //
+}
