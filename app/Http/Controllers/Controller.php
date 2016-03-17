b0a904b3b849f860ac68b43613e4cf21d29228b5
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 23:10:55 -0430

    Agregando metodos para retornar respuestas del servidor a peticiones http

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function setSuccess($success = false)
+	{
+		$this->responseArray['success'] = $success;
+	}
+
+	public function getResponseArray()
+	{
+		return $this->responseArray;
+	}
+
+	public function getResponseArrayJson()
+	{
+		return response()->json($this->responseArray);
+	}	
+
+	public function addToResponseArray($key, $value)
+	{
+		$this->responseArray[$key] = $value;
+	}
 }
