c8a3bbef9a1c1d05e32b1bab74cb4cac8acb010d
ajpadilla <ajpadilla88@gmail.com>
2016-03-16 23:13:36 -0430

    Reorganizando archivos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta charset="UTF-8">
 	<title>@yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
