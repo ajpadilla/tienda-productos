df1bffbe4b3eb36f85ba762b8c3a0d52e3f51f73
ajpadilla <ajpadilla88@gmail.com>
2016-03-19 01:58:59 -0430

    Agregando metodo para crear nueva clasificación de productos

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,44 @@
 new Vue({
 	el:'body',
 	data:{
-		showCustomModal: false
+		showCustomModal: false,
+		showResponseModal: false,
+		nameClassification: '',
+		errorsResponse: [],
+		messageResponse: '',
+		showErrors: false
+	},
+	methods:{
+		createNewClassification: function() 
+		{
+			if(this.$validation1.valid)
+			{
+				Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#_token').getAttribute('value');
+				this.$http.post('/admin/classification/store',{name: this.nameClassification}).then(function (response) {
+					this.errorsResponse = [];
+					this.showResponseModal = true;
+					this.messageResponse = 'Clasificación' +' '+ this.nameClassification +" "+'agregada correctamente';
+					this.nameClassification = '';
+				}, function (errors) {
+					this.showResponseModal = true;
+					var errorsR = [];
+					$.each(errors.data, function(index,value) {
+						errorsR.push(value);
+					} );
+					this.errorsResponse = errorsR;
+				});
+			}else{
+				this.showErrors = true;
+			}
+		},
+		closeCustomModal: function() {
+			this.showCustomModal = false;
+			this.showErrors = false;
+		}
+		,
+		closeResponseModal: function() {
+			this.showResponseModal = false;
+		}
 	},
 	components:{
 		modal: VueStrap.modal
